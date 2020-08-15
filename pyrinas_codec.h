018c4887d030f5962728e92e0152ed2427ab2153
Jared Wolff <hello@jaredwolff.com>
2020-08-15 17:11:28 -0400

    Fixing last issues with pyrinas-codec

--- pyrinas_codec.h
+++ pyrinas_codec.h
@@ -8,23 +8,23 @@ typedef struct
 {
     size_t size;
     uint8_t bytes[18];
-} ble_event_name_data_t;
+} pyrinas_event_name_data_t;
 
 typedef struct
 {
     size_t size;
     uint8_t bytes[128];
-} ble_event_data_t;
+} pyrinas_event_data_t;
 
 typedef struct
 {
-    ble_event_name_data_t name;
-    ble_event_data_t data;
+    pyrinas_event_name_data_t name;
+    pyrinas_event_data_t data;
     uint8_t faddr[6];
     uint8_t taddr[6];
 } pyrinas_event_t;
 
-#define pyrinas_event_t_size sizeof(pyrinas_event_t) + 64
+#define pyrinas_event_t_size 184 + 64
 
 int pyrinas_codec_encode(const pyrinas_event_t *p_data, uint8_t *p_buf, size_t len, size_t *p_size);
 int pyrinas_codec_decode(pyrinas_event_t *p_data, const uint8_t *p_buf, size_t len);
